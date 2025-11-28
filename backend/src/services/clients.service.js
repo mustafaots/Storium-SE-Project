4bbc75aff615c175fa1e87761e44166ead4efd3d
Mustapha OTSMANE <fafa.ots15@gmail.com>
2025-11-28 11:58:02 +0100

    reduced the client service by using client model instead

--- backend/src/services/clients.service.js
+++ backend/src/services/clients.service.js
@@ -1,63 +1,13 @@
-import connection from '../config/database.js';
+import { Client } from './clients.model.js';
 
 export const clientsService = {
-  // Get all clients
-  getAll: () => {
-    return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM clients ORDER BY created_at DESC', (err, results) => {
-        if (err) reject(err);
-        else resolve(results);
-      });
-    });
-  },
-
-  // Get client by ID
-  getById: (id) => {
-    return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM clients WHERE client_id = ?', [id], (err, results) => {
-        if (err) reject(err);
-        else resolve(results[0]);
-      });
-    });
-  },
-
-  // Create new client
-  create: (clientData) => {
-    return new Promise((resolve, reject) => {
-      const { client_name, contact_email, contact_phone, address } = clientData;
-      connection.query(
-        'INSERT INTO clients (client_name, contact_email, contact_phone, address) VALUES (?, ?, ?, ?)',
-        [client_name, contact_email, contact_phone, address],
-        (err, results) => {
-          if (err) reject(err);
-          else resolve({ client_id: results.insertId, ...clientData });
-        }
-      );
-    });
-  },
-
-  // Update client
-  update: (id, clientData) => {
-    return new Promise((resolve, reject) => {
-      const { client_name, contact_email, contact_phone, address } = clientData;
-      connection.query(
-        'UPDATE clients SET client_name = ?, contact_email = ?, contact_phone = ?, address = ? WHERE client_id = ?',
-        [client_name, contact_email, contact_phone, address, id],
-        (err, results) => {
-          if (err) reject(err);
-          else resolve(results);
-        }
-      );
-    });
-  },
-
-  // Delete client
-  delete: (id) => {
-    return new Promise((resolve, reject) => {
-      connection.query('DELETE FROM clients WHERE client_id = ?', [id], (err, results) => {
-        if (err) reject(err);
-        else resolve(results);
-      });
-    });
-  }
+  getAll: () => Client.getAll(),
+  
+  getById: (id) => Client.getById(id),
+  
+  create: (clientData) => Client.create(clientData),
+  
+  update: (id, clientData) => Client.update(id, clientData),
+  
+  delete: (id) => Client.delete(id)
 };
