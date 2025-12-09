dcb3ae5b711a0f039e125ace8a85aeb826c2d63d
MEZROUH INAME <iname.mezrouh@ensia.edu.dz>
2025-12-09 11:03:20 +0100

    add services and manual transactions endpoints

--- backend/src/routes/transactions.routes.js
+++ backend/src/routes/transactions.routes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { getTransactions } from '../controllers/transactions.controller.js';
+import { getTransactions , createManualOutflow ,createManualInflow,
+  createTransfer,
+  createAdjustment
+} from '../controllers/transactions.controller.js';
 
 const router = express.Router();
 
 router.get('/', getTransactions);
+router.post('/manual-outflow', createManualOutflow);
+router.post('/manual-inflow', createManualInflow);
+router.post('/transfer', createTransfer);
+router.post('/adjustment', createAdjustment);
 
 export default router;
